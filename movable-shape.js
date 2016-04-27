db60596a3eba66bb05130ed8bc62fe4dc5960034
Noah McMullen <noahmcmlln@gmail.com>
2016-04-27 19:04:05 -0400

    Get car moving
    Initial commit sans ground

--- movable-shape.js
+++ movable-shape.js
@@ -6,7 +6,7 @@ var movableShape = {
 	// origin is used to store the original position of the shape
 	origin: { x: 0, y: 0},
 	// delta.x and delta.y indicate how much to move() the shape
-	speed: { x: 0, y: 0 },
+	speed: { x: -5, y: 0},
 
 	/*
 		Put your shape(s) in drawShape()
--- sketch.js
+++ sketch.js
@@ -1,11 +1,33 @@
 // give movableShape a nice, descriptive name to work with here
 // (i.e., replace the variable name myShape with something more descriptive (e.g., myCar, myRocket).)
-var myShape = movableShape;
+var myCar = movableShape;
 
 // Redefine drawShape() by putting your moving shape in here
 // (Basically, you can copy and paste your code from last time, although you may wish to refactor it so that it's more readable.)
 // That said, you may wish to try putting your unrefactored, raw drawing code from week 1 in here
-myShape.drawShape = function () {
+myCar.drawShape = function () {
+	noStroke();
+	fill(184, 9, 32); // hood? body around windshield? roof? whatever the word is
+  ellipse(300, 250, 325, 200);
+
+  fill(193, 202, 232); // windshield
+  ellipse(300, 250, 300, 150);
+
+  fill(184, 9, 32); // car's body
+  ellipse(125, 300, 100, 100);
+  ellipse(475, 300, 100, 100);
+  rect(125, 250, 350, 100);
+
+  fill(0); // two tires
+  ellipse(175, 350, 75, 75);
+  ellipse(425, 350, 75, 75);
+
+  fill(150); // two hubcaps
+  ellipse(175, 350, 50, 50);
+  ellipse(425, 350, 50, 50);
+
+  fill(245, 241, 42); // headlights
+  ellipse(110, 300, 30, 30);
 	// your code goes here
 };
 
@@ -16,7 +38,7 @@ setup = function() {
 };
 
 draw = function() {
-  background(100); // refresh the background
-  myShape.display(); // display myShape
-  myShape.update(); // and then update it
+  background(200); // refresh the background
+  myCar.display(); // display myShape
+  myCar.update(); // and then update it
 };
