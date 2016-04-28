0fe45eeca410f1b2556f2e54f51fe0bde600e09f
Noah McMullen <noahmcmlln@gmail.com>
2016-04-27 20:59:06 -0400

    Movable Rocket
    Rocket that moves

--- sketch.js
+++ sketch.js
@@ -1,11 +1,35 @@
 // give movableShape a nice, descriptive name to work with here
-// (i.e., replace the variable name myShape with something more descriptive (e.g., myCar, myRocket).)
-var myShape = movableShape;
+// (i.e., replace the variable name myRocket with something more descriptive (e.g., myCar, myRocket).)
+var myRocket = movableShape;
 
 // Redefine drawShape() by putting your moving shape in here
 // (Basically, you can copy and paste your code from last time, although you may wish to refactor it so that it's more readable.)
 // That said, you may wish to try putting your unrefactored, raw drawing code from week 1 in here
-myShape.drawShape = function () {
+myRocket.drawShape = function () {
+	fill("red");
+  triangle(200, 500, 300, 300, 400, 500);
+  triangle(250, 200, 300, 100, 350, 200);
+
+  fill(150);
+  rect(250, 200, 100, 300);
+
+  fill(50);
+  ellipse(300, 250, 50, 50);
+  ellipse(300, 325, 50, 50);
+
+  fill(203, 236, 247);
+  ellipse(300, 250, 40, 40);
+  ellipse(300, 325, 40, 40);
+
+	fill(0);
+	textSize(32);
+	text("USA", 268, 450);
+
+  fill("yellow");
+  triangle(275, 500, 300, 575, 325, 500);
+
+  fill("red");
+  triangle(285, 500, 300, 550, 315, 500);
 	// your code goes here
 };
 
@@ -16,7 +40,7 @@ setup = function() {
 };
 
 draw = function() {
-  background(100); // refresh the background
-  myShape.display(); // display myShape
-  myShape.update(); // and then update it
+  background(0); // refresh the background
+  myRocket.display(); // display myRocket
+  myRocket.update(); // and then update it
 };
